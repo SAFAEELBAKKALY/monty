030e5e90ed0c628e8c1a5707afa42d3e93ee8e71
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-08-26 11:37:28 -0700

    update

--- opcodes_2.c
+++ opcodes_2.c
@@ -28,14 +28,14 @@ void opcode_mod(stack_t **stack, unsigned int line_number)
  */
 void opcode_pchar(stack_t **stack, unsigned int line_number)
 {
-	if ((*stack)->n < 0 || (*stack)->n > 127)
-	{
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
-		exit(EXIT_FAILURE);
-	}
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n < 0 || (*stack)->n > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	putchar((*stack)->n);
